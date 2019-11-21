a12fc2f66901836f62159dca054ccb80d25ab3d9
Dillon Smith <dillon.th.smith@gmail.com>
2019-11-21 16:11:38 -0500

    Significantly developed AST parsing algorithm. Now generally supports PNL scripts in which elements are not instantiated within loops.

--- src/py/scratch_AST_PARSE.py
+++ src/py/scratch_AST_PARSE.py
@@ -1,47 +1,61 @@
 from redbaron import RedBaron
 from enum import Enum
 
-fp = '/Users/ds70/Library/Preferences/PyCharm2019.2/scratches/scratch_AST_PARSE_EX_SCRIPT.py'
 fp = '/Users/ds70/PycharmProjects/PsyNeuLink/Scripts/Examples/Composition/Botvinick Model Composition.py'
-fp = '/Users/ds70/PycharmProjects/PsyNeuLink/Scripts/Examples/Composition/EVC OCM.py'
+# fp = '/Users/ds70/PycharmProjects/PsyNeuLink/Scripts/Examples/Composition/StabilityFlexibility.py'
+# fp = '/Users/ds70/PycharmProjects/PsyNeuLink/Scripts/Examples/Composition/EVC-Gratton Composition.py'
+# fp = '/Users/ds70/PycharmProjects/PsyNeuLink/Scripts/Examples/Composition/LC Control Mechanism Composition.py'
+# fp = '/Users/ds70/PycharmProjects/PsyNeuLink/Scripts/Examples/Composition/Rumelhart Semantic Network.py'
+
+ns = {}
+ns2 = {}
 
 class DependencyTypes(Enum):
     COMPOSITION_MANIPULATION = 0
     COMPOSITION = 1
     MECHANISM = 2
-    MECHANISM_DEPENDENCY = 3
+    PROJECTION = 3
     PERIPHERAL_DEPENDENCY = 4
-
 
 class _DependencyNode:
     def __init__(self, fst_node):
         self.dependencies = []
+        self.dependents = []
         self.fst_node = fst_node
 
     def add_dependency(self, dependency):
         if not dependency in self.dependencies:
             self.dependencies.append(dependency)
 
+    def add_dependent(self, dependent):
+        if not dependent in self.dependents:
+            self.dependents.append(dependent)
+
+    def __repr__(self):
+        return self.fst_node.dumps()
 
 class DependencyGraph:
     def __init__(self, fp, psyneulink_instance):
         self.psyneulink_instance = psyneulink_instance
         self.psyneulink_composition_classes = self.get_class_hierarchy(self.psyneulink_instance.Composition)
         self.psyneulink_mechanism_classes = self.get_class_hierarchy(self.psyneulink_instance.Mechanism)
+        self.psyneulink_projection_classes = self.get_class_hierarchy(self.psyneulink_instance.Projection)
         self.psyneulink_function_classes = self.get_class_hierarchy(self.psyneulink_instance.Function)
         self.imports = []
         self.path_operations = []
+        self._assignments = {}
         self._compositions = set()
         self._composition_manipulations = set()
         self._mechanisms = set()
+        self._projections = set()
         self._mechanism_dependencies = set()
         self._peripheral_dependencies = set()
         self._dependency_method_map = {
             DependencyTypes.COMPOSITION: self.add_composition,
             DependencyTypes.COMPOSITION_MANIPULATION: self.add_composition_manipulation,
             DependencyTypes.MECHANISM: self.add_mechanism,
-            DependencyTypes.MECHANISM_DEPENDENCY: self.add_mechanism_dependency,
-            DependencyTypes.PERIPHERAL_DEPENDENCY: self.add_peripheral_dependency
+            DependencyTypes.PROJECTION: self.add_projection,
+            DependencyTypes.PERIPHERAL_DEPENDENCY: self.add_peripheral_dependency,
         }
         self.manipulation_methods = [
             'add_node',
@@ -59,6 +73,8 @@ class DependencyGraph:
         self.graph_dict = ''
         self.filepath = fp
         self.fst = RedBaron(open(self.filepath, 'r').read())
+        self.all_assigns = self.fst.find_all('assign')
+        self.all_assigns_dict = {k:v for k,v in [(i.name.value,i) for i in self.all_assigns]}
         self.parse_fst()
 
     def get_class_hierarchy(self, root_class, class_hierarchy = None):
@@ -73,11 +89,12 @@ class DependencyGraph:
 
     def parse_fst(self):
         self.extract_imports()
+        self.extract_defs()
         self.extract_path_operations()
         self.extract_compositions()
         self.extract_composition_manipulations()
         self.extract_mechanisms()
-        self.extract_mechanism_dependencies()
+        self.extract_projections()
         self.extract_peripheral_dependencies()
 
     def extract_imports(self):
@@ -85,6 +102,26 @@ class DependencyGraph:
             "fromimport",
             "import"
         ])
+        # for imp in self.imports:
+        #     dotted_as_name = imp.find('dotted_as_name')
+        #     if dotted_as_name:
+        #         if not dotted_as_name.target:
+        #             self.add_assignment(dotted_as_name.dumps(), imp)
+        #         else:
+        #             self.add_assignment(dotted_as_name.target, imp)
+        #     else:
+        #         name_as_name = imp.find('name_as_name')
+        #         if name_as_name:
+        #             self.add_assignment(name_as_name.target, imp)
+        for imp in self.imports:
+            exec(imp.dumps(),ns)
+
+    def extract_defs(self):
+        self.defs = self.fst.find_all('def',
+                                      lambda x: x if len(x.path().path)==1 else False)
+        for _def in self.defs:
+            self.add_assignment(_def.name, _def)
+        assert True
 
     @property
     def compositions(self):
@@ -111,11 +148,25 @@ class DependencyGraph:
         self._mechanisms.update(mechanisms)
 
     @property
+    def projections(self):
+        return list(self._projections)
+
+    def add_projection(self, projection):
+        self._projections.add(projection)
+
+    @property
     def mechanism_dependencies(self):
         return list(self._mechanism_dependencies)
 
     def add_mechanism_dependency(self, mechanism_dependency):
         self._mechanism_dependencies.add(mechanism_dependency)
+
+    @property
+    def assignments(self, assignment):
+        return self._assignments[assignment]
+
+    def add_assignment(self, assignment, node):
+        self._assignments[assignment] = node
 
     @property
     def peripheral_dependencies(self):
@@ -127,8 +178,10 @@ class DependencyGraph:
     def add_dependency_node(self,
                             fst_node,
                             dependency_type,
-                            dependee=None,
-                            dependent=None):
+                            dependent=None,
+                            dependency=None):
+        if fst_node.type == 'assignment':
+            self.add_assignment(fst_node.name.value, fst_node)
         if not fst_node in self.index:
             dn = _DependencyNode(fst_node)
             self.index[fst_node] = {
@@ -138,10 +191,12 @@ class DependencyGraph:
             self._dependency_method_map[dependency_type](dn)
         else:
             dn = self.index[fst_node]['node']
-        if dependee:
-            dependee.add_dependency(dn)
         if dependent:
-            dn.add_dependency(dependent)
+            dn.add_dependent(dependent)
+            dependent.add_dependency(dn)
+        if dependency:
+            dn.add_dependency(dependency)
+            dependency.add_dependent(dn)
         return dn
 
     def extract_path_operations(self):
@@ -171,17 +226,58 @@ class DependencyGraph:
                 self.add_dependency_node(
                         call,
                         DependencyTypes.COMPOSITION_MANIPULATION,
-                        dependent=composition_node
+                        dependency=composition_node
                 )
 
-    def unpack_call_node(self, call_node, tokens = None):
-        if not tokens:
+    def get_peripheral_dependencies(self, node):
+        # node_unpacked = [i for i in self.unpack_node(node) if not i in self.assignments and not i in ns]
+        node_unpacked = [i for i in self.unpack_node(node)]
+        if node_unpacked:
+            for i in node_unpacked:
+                if i in self._assignments:
+                    assignment = self._assignments[i]
+                elif i in self.all_assigns_dict:
+                    assignment =  self.all_assigns_dict[i]
+                else:
+                    assignment = None
+                if assignment:
+                    self.add_dependency_node(
+                            assignment,
+                            DependencyTypes.PERIPHERAL_DEPENDENCY,
+                            dependent=self.index[node]['node']
+                    )
+                    self.get_peripheral_dependencies(assignment)
+        pass
+
+    def handle_atom_trailers(self, node, tokens):
+        tokens.append(node.name.value)
+        for j in [i for i in node.value if i.type == 'getitem' or i.type == 'call']:
+            self.unpack_node(j, tokens)
+
+    def unpack_node(self, node, tokens = None):
+        # if AtomtrailersNode, check first name node and unpack GetitemNodes and CallNodes
+        if tokens is None:
             tokens = []
         try:
-            for i in call_node.value:
-                tokens = self.unpack_call_node(i, tokens)
+            if node.type == 'atomtrailers':
+                self.handle_atom_trailers(node, tokens)
+            elif hasattr(node.value,'type') \
+                and node.value.type == 'atomtrailers':
+                self.handle_atom_trailers(node.value, tokens)
+            else:
+                for i in node.value:
+                    if hasattr(i.value,'type') \
+                            and i.value.type == 'atomtrailers':
+                        self.handle_atom_trailers(i.value, tokens)
+                    elif isinstance(i.value, str):
+                        tokens.append(i.value)
+                        continue
+                    else:
+                        self.unpack_node(i, tokens)
         except (TypeError, AttributeError):
-            tokens.append(call_node.value.value)
+            if not node.value.type in ['int','float','binary',
+                                       'string','raw_string','binary_string']:
+                tokens.append(node.value.dumps())
         return tokens
 
     def extract_mechanisms(self):
@@ -193,36 +289,72 @@ class DependencyGraph:
                                                 else False)
         for composition_manipulation in self.composition_manipulations:
             call = composition_manipulation.fst_node.find('call')
-            test = self.unpack_call_node(call)
-            call_args = call.find_all('call_argument')
-            try:
-                call_args_iterable = [arg for arg in call_args]
-            except TypeError:
-                call_args_iterable = [call_args]
-            for arg in call_args_iterable:
-                if arg.value.type == 'list':
-                    arg_iterable = [i.value for i in arg.value]
-                else:
-                    arg_iterable = [arg.value]
-                for i in arg_iterable:
-                    mechanism_assignment_node = script_mechanisms.find(
-                            "assign",
-                            lambda x: x if x.find_all('namenode', i.value)
-                            else False
+            call_args = self.unpack_node(call)
+            for arg in call_args:
+                mechanism_assignment_node = script_mechanisms.find(
+                        "assign",
+                        lambda x: x if x.find_all('namenode', arg)
+                        else False
+                )
+                if mechanism_assignment_node:
+                    self.add_dependency_node(
+                            mechanism_assignment_node,
+                            DependencyTypes.MECHANISM,
+                            dependent=composition_manipulation
                     )
-                    if mechanism_assignment_node:
-                        self.add_dependency_node(
-                                mechanism_assignment_node,
-                                DependencyTypes.MECHANISM,
-                                dependee=composition_manipulation
-                        )
+
+    def extract_projections(self):
+        script_mechanisms = self.fst.find_all("assign",
+                                                lambda x: x if x.find_all("atomtrailers",
+                                                                          lambda x: x if x.find_all('namenode',
+                                                                                                    self.psyneulink_projection_classes)
+                                                                          else False)
+                                                else False)
+        for composition_manipulation in self.composition_manipulations:
+            call = composition_manipulation.fst_node.find('call')
+            call_args = self.unpack_node(call)
+            for arg in call_args:
+                projection_assignment_node = script_mechanisms.find(
+                        "assign",
+                        lambda x: x if x.find_all('namenode', arg)
+                        else False
+                )
+                if projection_assignment_node:
+                    self.add_dependency_node(
+                            projection_assignment_node,
+                            DependencyTypes.PROJECTION,
+                            dependent=composition_manipulation
+                    )
 
     def extract_mechanism_dependencies(self):
         pass
 
     def extract_peripheral_dependencies(self):
-        pass
+        primary_nodes = list(self.index.keys())
+        for node in primary_nodes:
+            self.get_peripheral_dependencies(node)
 
 import psyneulink
+import time
 
+a = time.time()
 dg = DependencyGraph(fp, psyneulink)
+b = time.time()
+print(b-a)
+
+
+for imp in dg.imports:
+    exec(imp.dumps(),ns2)
+
+def traverse_graph(node):
+    if node.dependencies:
+        for i in node.dependencies:
+            traverse_graph(i)
+    if not dg.index[node.fst_node]['instantiated']:
+        exec(node.fst_node.dumps(), ns2)
+        dg.index[node.fst_node]['instantiated'] = True
+
+for i in dg.compositions[0].dependents:
+    traverse_graph(i)
+
+ns2[dg.compositions[0].fst_node.name.value].show_graph(show_learning=True)
